0099439cda51a36d42cff198993aef600c6d0b68
samyuktha <gollapallisamyuktha111@gmail.com>
2023-05-18 12:15:21 +0530

    Add test-1 file

--- app/src/main/java/com/learning/dialogfragments/MyDialogFragment.kt
+++ app/src/main/java/com/learning/dialogfragments/MyDialogFragment.kt
@@ -1,8 +1,6 @@
 package com.learning.dialogfragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -18,7 +16,6 @@ class MyDialogFragment : DialogFragment() {
     lateinit var buttonCancel:Button
 
 
-    @SuppressLint("CutPasteId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
---
+++ app/src/main/java/com/learning/dialogfragments/test1.kt
@@ -0,0 +1,5 @@
+package com.learning.dialogfragments
+
+class test1 {
+    
+}
